3520dfd892e896422ccce2a5eef3f4911f39100d
Mohamed Hassan <mohamedhassanmn>
2019-11-15 12:53:04 +0530

    partial-submission-2

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,12 +1,13 @@
 import React,{useEffect,useState} from 'react';
 import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button'
 import Paper from '@material-ui/core/Paper';
 import InputBase from '@material-ui/core/InputBase';
-import Divider from '@material-ui/core/Divider';
+import Display from './Display'
 import IconButton from '@material-ui/core/IconButton';
 import axios from 'axios'
 import SearchIcon from '@material-ui/icons/Search';
-import DirectionsIcon from '@material-ui/icons/Directions';
+import Grid from '@material-ui/core/Grid'
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -31,29 +32,45 @@ const useStyles = makeStyles(theme => ({
 export default function Search() {
   const classes = useStyles();
   const [data,setData]=React.useState("")
-  const [searchFor,setSearchFor]=React.useState("")
-  const handleAxios=()=>{
-    axios({method:"get",url:"http://www.omdbapi.com/?apikey=215e4f09&t=Game of Thrones&Season=1"})
-    .then(res=>console.log(res))
+  const handleAxios=(search)=>{
+    axios({method:"get",url:`http://www.omdbapi.com/?apikey=215e4f09&s=${search}&page=1`})
+    .then(res=>{
+      console.log(res)
+      setData(res.data.Search)      
+    })
     .catch(err=>alert(err))
     }
-
+  const handleChange=(e)=>{
+    handleAxios(e.target.value)
+  }
   React.useEffect(()=>{
       console.log("executed")
       if(data==""){
-        handleAxios()
+        handleAxios("Harry Potter")
       }
   })
+  console.log(data,"here")
 
   return (
-    <Paper component="form" className={classes.root}>
-      <InputBase
-        className={classes.input}
-        placeholder="Search For Movies"
-      />
-      <IconButton type="submit" className={classes.iconButton} aria-label="search">
-        <SearchIcon />
-      </IconButton>
-    </Paper>
+    <React.Fragment>
+      <Paper component="form" className={classes.root}>
+        <InputBase
+        onChange={handleChange}
+          className={classes.input}
+          placeholder="Search For Movies"
+        />
+        <IconButton type="submit" className={classes.iconButton} aria-label="search">
+          <SearchIcon />
+        </IconButton>
+      </Paper>
+      <br/> <br/> <br/> <br/>
+      <Grid container>
+        {data!==""?(<Display data={data}/>):(null)}
+      </Grid>
+      <br/> <br/>
+      <Button color="primary" variant="contained">Previous</Button>
+      &nbsp; &nbsp; &nbsp;
+      <Button color="secondary" variant="contained">Next</Button>
+    </React.Fragment>
   );
 }
